09b9866a92b34cb6525fe9c706ce77779702fd08
construemart <72640763+construemart@users.noreply.github.com>
2020-10-22 21:37:48 +0530

    files

---
+++ precache-manifest.99bd0b4f74ce1444a6084922431fc777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0030977f4ba5f23d27fafb887d8b52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fab8f3c1b8bc6bf2856",
+    "url": "/static/css/main.d6f98977.chunk.css"
+  },
+  {
+    "revision": "27963283fc1bc5de2456",
+    "url": "/static/js/2.5cc8d483.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.5cc8d483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fab8f3c1b8bc6bf2856",
+    "url": "/static/js/main.ec23fa23.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
